bd26ade60ce337a8ed0b2498bd24ebffa6134538
Richard Purdie <richard.purdie@linuxfoundation.org>
2021-07-18 12:59:15 +0100

    bitbake: data_smart/parse: Allow ':' characters in variable/function names
    It is becomming increasingly clear we need to find a way to show what
    is/is not an override in our syntax. We need to do this in a way which
    is clear to users, readable and in a way we can transition to.
    
    The most effective way I've found to this is to use the ":" charater
    to directly replace "_" where an override is being specified. This
    includes "append", "prepend" and "remove" which are effectively special
    override directives.
    
    This patch simply adds the character to the parser so bitbake accepts
    the value but maps it back to "_" internally so there is no behaviour
    change.
    
    This change is simple enough it could potentially be backported to older
    version of bitbake meaning layers using the new syntax/markup could
    work with older releases. Even if other no other changes are accepted
    at this time and we don't backport, it does set us on a path where at
    some point in future we could
    require a more explict syntax.
    
    I've tested this patch by converting oe-core/meta-yocto to the new
    syntax for overrides (9000+ changes) and then seeing that builds
    continue to work with this patch.
    
    (Bitbake rev: a6d5fb7554e3cf071e453db56a1e7469ac44277c)
    
    Signed-off-by: Richard Purdie <richard.purdie@linuxfoundation.org>

--- bitbake/lib/bb/parse/ast.py
+++ bitbake/lib/bb/parse/ast.py
@@ -114,6 +114,7 @@ class DataNode(AstNode):
     def eval(self, data):
         groupd = self.groupd
         key = groupd["var"]
+        key = key.replace(":", "_")
         loginfo = {
             'variable': key,
             'file': self.filename,
@@ -224,6 +225,7 @@ class ExportFuncsNode(AstNode):
     def eval(self, data):
 
         for func in self.n:
+            func = func.replace(":", "_")
             calledfunc = self.classname + "_" + func
 
             if data.getVar(func, False) and not data.getVarFlag(func, 'export_func', False):
